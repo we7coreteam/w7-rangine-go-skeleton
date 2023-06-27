14bd77abc8798e6d5eb1e308342708da0787b4fa
donknap <donknap@gmail.com>
2023-06-27 17:12:03 +0800

    修复小错误

--- main.go
+++ main.go
@@ -28,7 +28,7 @@ func main() {
 	// 业务中需要使用 http server，这里需要先实例化
 	httpServer := new(http.Provider).Register(app.GetConfig(), app.GetConsole(), app.GetServerManager()).Export()
 	// 注册一些全局中间件，路由或是其它一些全局操作
-	httpServer.Use(middleware.GetPanicHandlerMiddleware(nil))
+	httpServer.Use(middleware.GetPanicHandlerMiddleware())
 	httpServer.RegisterRouters(func(engine *gin.Engine) {
 		engine.GET("favicon.ico", func(context *gin.Context) {
 			response, _ := http2.Get("https://g.csdnimg.cn/static/logo/favicon32.ico")
